196cc08da34c28038a07460e77f884feafe31578
susegroj <jor_ge_711@hotmail.com>
2021-09-21 00:50:00 -0500

    :art: Installer / Webpack commons

---
+++ bin/cli.js
@@ -0,0 +1,45 @@
+#! usr/bin/env node
+
+const { execSync } = require('child_process');
+
+
+const runCommand = (command) => {
+  try {
+    execSync(command, { stdio: 'inherit' });
+  } catch (error) {
+    console.error(`Failed to executed ${command}`,  error);
+    return false;
+  }
+
+  return true;
+};
+
+const repoName = process.args[2];
+const gitCommand = `git clone --depth 1 'git@github.com:susegroj/react-starter-pack.git' ${repoName}`;
+
+const installDepsCommand = `cd ${repoName} && yarn install`;
+
+console.log(`⏱ Cloning the repository ${repoName}...`);
+
+const checkedOut = runCommand(gitCommand);
+
+if (!checkedOut) {
+  console.log('✖ Failed to clone the repository 🥺');
+  process.exit(-1);
+}
+
+console.log('✔ Cloned the repository 🎉');
+console.log(`🚧 Installing dependencies for ${repoName}`);
+
+const installDeps = runCommand(installDepsCommand);
+
+if (!installDeps) {
+  console.log('✖ Failed to install dependencies 🥺');
+  process.exit(-1);
+}
+
+console.log(`✅ Successfully cloned and installed ${repoName} 🤓`);
+
+console.log(`cd ${repoName} && yarn development`);
+
+console.log('🚀 Done! 🚀');
--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,100 +1,18 @@
 const path = require('path');
 const webpack = require('webpack');
 
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { merge } = require('webpack-merge');
 
-const Dotenv = require('dotenv-webpack');
+const common = require('./webpack.common');
 
-module.exports = {
+const host = process.env.HOST || '0.0.0.0';
+const port = parseInt(process.env.PORT, 10) || 3005;
+
+module.exports = merge(common,{
   target: 'web',
-  entry: '/src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    publicPath: '/',
-    filename: 'bundle.[contenthash].js',
-    assetModuleFilename: 'assets/images/[hash][ext][query]',
-  },
   mode: 'development',
   devtool: 'source-map',
-  resolve: {
-    extensions: ['.js', '.jsx'],
-    alias: {
-      '@components': path.resolve(__dirname, 'src/components/'),
-      '@pages': path.resolve(__dirname, 'src/pages/'),
-      '@context': path.resolve(__dirname, 'src/context/'),
-      '@hooks': path.resolve(__dirname, 'src/hooks/'),
-      '@utils': path.resolve(__dirname, 'src/utils/'),
-      '@styles': path.resolve(__dirname, 'src/styles/'),
-      '@images': path.resolve(__dirname, 'src/assets/images/'),
-      '@fonts': path.resolve(__dirname, 'src/assets/fonts/'),
-    },
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-        },
-      },
-      {
-        /* For react is required the HTML loader */ test: /\.html$/i,
-        use: [
-          {
-            loader: 'html-loader',
-          },
-        ],
-      },
-      {
-        test: /\.css|.s(a|c)ss$/i,
-        use: [
-          'style-loader',
-          'css-loader',
-          {
-            loader: 'sass-loader',
-            options: {
-              sourceMap: true,
-              sassOptions: {
-                outputStyle: 'compressed',
-              },
-            },
-          },
-        ],
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        use: {
-          loader: 'url-loader',
-          options: {
-            /* to enable or disable base64 files */
-            limit: 10000,
-            mimetype: 'application/font-woff',
-            name: '[name].[contenthash].[ext]',
-            outputPath: './assets/fonts',
-            publicPath: '../assets/fonts',
-            esModule: false,
-          },
-        },
-      },
-    ],
-  },
   plugins: [
-    new HtmlWebpackPlugin({
-      inject: true,
-      template: './public/index.html',
-      filename: './index.html',
-      favicon: './src/assets/favicon.svg',
-    }),
-    new MiniCssExtractPlugin({
-      filename: 'assets/build.[contenthash].css',
-    }),
-    new Dotenv(),
     new webpack.HotModuleReplacementPlugin({
       multiStep: true,
     }),
@@ -102,9 +20,9 @@ module.exports = {
   devServer: {
     contentBase: path.join(__dirname, 'dist'),
     compress: true,
-    port: 3005,
-    host: '0.0.0.0',
+    port,
+    host,
     historyApiFallback: true,
     open: true,
   },
-};
+});
